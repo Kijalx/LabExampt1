209c6449773439cdd69e30f0d7b7a55988486f64
Aleks <G00396018@atu.ie>
2023-11-08 09:28:33 +0000

    Added addProduct and getProductbyId

--- src/main/java/ie/atu/labexam/ProductController.java
+++ src/main/java/ie/atu/labexam/ProductController.java
@@ -1,4 +1,26 @@
 package ie.atu.labexam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/addprod")
+    public String addProduct(@RequestBody ProductData productData){
+        productService.addProduct(productData);
+        return "Added book";
+    }
+
+    @GetMapping("/getprod/{id}")
+    public ProductData getProductById(@PathVariable int id){
+        return productService.getProductByID(id);
+    }
+
 }
--- src/main/java/ie/atu/labexam/ProductService.java
+++ src/main/java/ie/atu/labexam/ProductService.java
@@ -1,4 +1,18 @@
 package ie.atu.labexam;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProductService {
+    private List<ProductData> product = new ArrayList<>();
+    public void addProduct(ProductData productData){
+        product.add(productData);
+    }
+
+    public ProductData getProductByID(int id){
+        return product.get(id);
+    }
 }
